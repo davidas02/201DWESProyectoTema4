84e2b48900e0d500373b777092ca130da159e887
David Aparicio Sir <davidapariciosir@gmail.com>
2022-12-02 12:14:45 +0100

    02/12/2022 Modificaciones index y creacion BD Borrado mostrar Creacion Trabajo en clase

--- indexProyectoTema4.php
+++ indexProyectoTema4.php
@@ -40,6 +40,11 @@
                         <img src="doc/img/mysql.png" alt="alt"/>
                     </a>
                 </td>
+                <td>
+                    <a href="mostrarCodigo/mostrarCreacionBDDesarrollo.php" >
+                        <img src="doc/img/mysql.png" alt="alt"/>
+                    </a>
+                </td>
             </tr>
             <tr>
                 <th>Carga Inicial</th>
